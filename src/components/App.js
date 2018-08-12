504c2179d749d0223e70ddb6691c81d24a131b66
huichingheng <huichingheng@gmail.com>
2018-08-13 01:55:15 +0800

    center the header

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hacker News Reader</h1>
+        <h1 align="center">Hacker News Reader</h1>
         <NewsFeed />
       </div>
     );
