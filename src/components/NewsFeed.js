b2a61c2aec90fd918aad1e57ec80d2d405b4d31c
huichingheng <huichingheng@gmail.com>
2018-08-13 02:02:05 +0800

    move each article field to individual component

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-// import Article from "./Article";
 import { articles } from "../seedData.js";
+import Article from "./Article";
 
 class NewsFeed extends Component {
   constructor() {
@@ -13,40 +13,22 @@ class NewsFeed extends Component {
   render() {
     return this.state.articles.map((article, i) => {
       return (
-        <div className="box" key={i}>
-          <img src={article.urlToImage} alt={article.description} />
-          <h2>{article.title}</h2>
-          <h3>{article.author}</h3>
-          <p>{article.description}</p>
-          <p>
-            <strong>Published at:</strong> {article.publishedAt} <br />{" "}
-            <strong>Vote:</strong> {article.votes}{" "}
-            <button onClick={() => this.handleClick(i)}>vote</button>
-          </p>
-
-          <a href={article.url}>Link:</a>
-        </div>
+        <Article
+          key={i}
+          article={article}
+          index={i}
+          handleClick={(index, option) => this.handleClick(index, option)}
+        />
       );
     });
   }
 
-  handleClick(index) {
-    // 1. console.log the article that was clicked
-    console.log(index);
+  handleClick(index, option) {
+    const articleToUpdate = this.state.articles;
+    if (option === "upvote") articleToUpdate[index].votes++;
+    else if (option === "downvote") articleToUpdate[index].votes--;
 
-    // 2. update the number of votes in the clicked article
-    const articleToUpdate = this.state.articles[index];
-    articleToUpdate.votes = articleToUpdate.votes + 1;
-
-    // 3. create new array with 1 updated article
-    const updatedArticles = [
-      ...this.state.articles.slice(0, index),
-      articleToUpdate,
-      ...this.state.articles.slice(index + 1)
-    ];
-
-    // 4. call this.setState({...}) to update the articles array with the new array
-    this.setState({ articles: updatedArticles });
+    this.setState({ articles: articleToUpdate });
   }
 }
 
