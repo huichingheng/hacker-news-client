f7d0d394e47af7a5d2eecaaa60bbbbdb131ddc53
huichingheng <huichingheng@gmail.com>
2018-08-13 02:02:27 +0800

    gather each component here

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import Image from "./Image";
+import Title from "./Title";
+import Voting from "./Voting";
+import Details from "./Details";
+import Description from "./Description";
 
-class Article extends Component {
-  render() {
-    return (
+const Article = props => {
+  return (
+    <div className="container">
       <div>
-        <li>Some Article </li>
-        
+        <Image urlToImage={props.article.urlToImage} />
       </div>
-    );
-  }
-}
+      <div className="content">
+        <Title title={props.article.title} />
+        <Details
+          author={props.article.author}
+          publishedAt={props.article.publishedAt}
+          votes={props.article.votes}
+        />
+        <Voting
+          index={props.index}
+          handleClick={(index, option) => props.handleClick(index,option)}
+        />
+        <Description
+          description={props.article.description}
+          url={props.article.url}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Article;
